7de4a025514058c77481a7579a33f12f5f2edb54
Jonathan Harris <jharris319@gmail.com>
2015-05-10 23:57:07 -0500

    updated app icon

--- app/src/main/java/org/jdharris/isotherm/WeatherActivity.java
+++ app/src/main/java/org/jdharris/isotherm/WeatherActivity.java
@@ -1,17 +1,24 @@
 package org.jdharris.isotherm;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class WeatherActivity extends ActionBarActivity {
+    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        LocationManager locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        Toast.makeText(this, Double.toString(location.getLatitude()), Toast.LENGTH_SHORT).show();
     }
 
     @Override
